ede77d220546d25db100c8e108c18503c55a0dff
Coleman <charleebrown22@gmail.com>
2022-08-18 15:29:00 -0500

    adding mongodb

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,43 +1,19 @@
-let date = new Date();
-let nowDate = date.getDate();
 let moment = require('moment');
-console.log(moment().format("MMMM Do, YYYY"));
+console.log(moment().add(14).format("MMMM Do, YYYY"));
+let nowDate = new Date();
+const changeDate = moment(nowDate).add(14, 'd').format("MMMM Do, YYYY");
 
-class Person{
-	constructor(name, age, friends) {
-		this.name = name;
-		this.age = age;
-		this.friends = friends;
-	}
-	getFriends(){
-		console.log("you currently have...");
-		if(this.friends <1){
-			console.log(this.friends + " - Friends. Jeez");
-		}
-	}
-		}
-
-
+console.log(changeDate);
+console.log(moment().diff(changeDate, moment()));
 function getMill(days){
 	let timeNum = days * 24 * 60 * 60 * 1000;
 	return timeNum;
 }
 
 function setDate(){
-	let seeDate = new Date();
-	let checkDate = new Date();
-	checkDate.setDate(seeDate.getDate() + 14);
-	console.log(checkDate);
-	seeDate.setDate(seeDate.getDate() + 14);
-	console.log(seeDate);
 
-	// if(seeDate.toLocaleDateString === checkDate.toLocaleDateString){
-	// 	console.log(seeDate);
-	// }
-	// else{
-	// 	console.log("wrong");
-	// }
-	
+	let checkDate = new Date();
+	checkDate.setDate(checkDate.getDate() + 14);
 }
 
 
