fe53536f1beaeb697936499965317bc09655323f
Coleman <charleebrown22@gmail.com>
2022-08-18 15:38:05 -0500

    found countdowqn in node

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,25 +1,15 @@
 let moment = require('moment');
-console.log(moment().add(14).format("MMMM Do, YYYY"));
 let nowDate = new Date();
 const changeDate = moment(nowDate).add(14, 'd').format("MMMM Do, YYYY");
 
-console.log(changeDate);
-console.log(moment().diff(changeDate, moment()));
-function getMill(days){
-	let timeNum = days * 24 * 60 * 60 * 1000;
-	return timeNum;
-}
+// Get a list
+// set the maximum option # to the list total.
+// use the rng to pick an option.
+// 
 
-function setDate(){
-
-	let checkDate = new Date();
-	checkDate.setDate(checkDate.getDate() + 14);
-}
-
-
-function getNumber(maximum, minimum){
+function optionPick(maximum, minimum){
 	let option = Math.round(Math.random() * (maximum - minimum + 1),0);
 	return option;
 }
 
-console.log(getNumber(90,0))
+
