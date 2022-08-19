18fbc95a0d213513d4c07e3a941fa3b7fb0c0929
Coleman <charleebrown22@gmail.com>
2022-08-19 11:53:00 -0500

    Changed to birthday countdown

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,50 +1,47 @@
-let moment = require('moment');
-let nowDate = new Date();
-const changeDate = moment(nowDate).add(14, 'd').format("MMMM Do, YYYY");
+
+let nowDate = new Date()
+// const changeDate = moment(nowDate)
+//   .add(14, 'd')
+//   .format('MMMM Do, YYYY')
 
 // Get a list
 // set the maximum option # to the list total.
 // use the rng to pick an option.
-// 
+//
 
-function optionPick(maximum, minimum){
-	let option = Math.round(Math.random() * (maximum - minimum + 1),0);
-	return option;
+function optionPick (maximum, minimum) {
+  let option = Math.round(Math.random() * (maximum - minimum + 1), 0)
+  return option
 }
 
-function newDate(){
+let counts = setInterval(function () {
+  // Set the date we're counting down to
+  let countDownDate = new Date('Aug 28, 2022');
 
-	
-}
-let counts = setInterval(function(){
-	// Set the date we're counting down to
-let countDate = moment("Aug 28, 2022");
+  // Update the count down every 1 second
+ 
+    // Get today's date and time
+    var now = new Date().getTime()
 
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    )
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+    // Output the result in an element with id="demo"
+    document.getElementById('demo').innerHTML =
+      days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds +'s'
 
-})
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(x)
+      document.getElementById('demo').innerHTML = 'EXPIRED'
+    }
+  
+},1000)
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var moment = require('moment');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
   res.render('index', { title: 'Express' });
 });
 
