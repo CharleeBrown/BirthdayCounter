bad32fbafb30e34fa325ad1924a12f67307d09ad
Coleman <charleebrown22@gmail.com>
2022-08-19 16:04:07 -0500

    added connection. setting up function to save

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,24 @@ require('dotenv').config();
 let pw = process.env.password;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://lee:"+pw +"@cluster0.guc9f.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const db = client.db("misc");
+const coll = db.collection("bday");
+
+
+function connectToDb(name, message){
+
+  client.connect(err => {
+    coll.insertOne({name:"test"});
+  });
+
+  client.close();
+  }
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
   res.render('index', { title: 'Xi-Day Counter' });
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("misc").collection("bday");
-  // perform actions on the collection object
-  client.close();
-});
+  connectToDb();
 });
 
 router.post('/', function(req, res, next) {
@@ -23,5 +31,6 @@ router.post('/', function(req, res, next) {
 
 
 })
+
 
 module.exports = router;
